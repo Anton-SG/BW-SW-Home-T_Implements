0710b98c7d9ea09975fd2951793dc56b655ab174
Anton-SG <137610845+Anton-SG@users.noreply.github.com>
2023-06-25 22:43:44 +0300

    Update Fuel.h

--- Inc/Fuel.h
+++ Inc/Fuel.h
@@ -40,11 +40,11 @@ public:
 	*@method		: constructor
 	********************************************************************/
 	Fuel()
-    { 
+    	{ 
 		mFuelType = eFUEL_NOT_SUPPORTED;
 		mFuelLevel = 0; 
 		mMunimalLEvelToStop = MINIMAL_FUEL_LEVEL_TO_FORCE_STOP;
-    }
+    	}
 
 	/********************************************************************
 	*@method		: constructor
@@ -55,35 +55,35 @@ public:
 	*@method		: Set Implement State
 	********************************************************************/
 	bool UpdateLevel(uint8_t aLevel) 
-    { 
-        mFuelLevel = aLevel; //Check level correctness must be added
+    	{ 
+        	mFuelLevel = aLevel; //Check level correctness must be added
 
-        //return if the level is ok
-        return (mFuelLevel >= mMunimalLEvelToStop);
-    }
+        	//return if the level is ok
+        	return (mFuelLevel >= mMunimalLEvelToStop);
+    	}
 
 	/********************************************************************
 	*@method		: Init
 	********************************************************************/
 	void Init(FUEL_TYPE aType, uint8_t aLevel, uint8_t aMinLevel = MINIMAL_FUEL_LEVEL_TO_FORCE_STOP) 
-    {
-        //Check input correctness if needed
+    	{
+        	//Check input correctness if needed
 		mFuelType = aType;
-        mFuelLevel = aLevel; 
-        mMunimalLEvelToStop = aMinLevel;
-    }
+        	mFuelLevel = aLevel; 
+        	mMunimalLEvelToStop = aMinLevel;
+    	}
 
 	/********************************************************************
 	*@method		: GetFuelLevel
 	********************************************************************/
 	uint8_t GetFuelLevel() { return mFuelLevel; }
 
-    /********************************************************************
+    	/********************************************************************
 	*@method		: SetFuelType
 	********************************************************************/
 	void SetFuelType(FUEL_TYPE aType) { mFuelType = aType; }
 
-    /********************************************************************
+    	/********************************************************************
 	*@method		: CheckAndUpdateFuelType
 	********************************************************************/
 	uint8_t CheckAndUpdateFuelLevel() 
@@ -92,18 +92,15 @@ public:
 		return mFuelLevel;
 	}
 
-    /********************************************************************
+    	/********************************************************************
 	*@method		: GetFuelType
 	********************************************************************/
 	FUEL_TYPE GetFuelType() { return mFuelType; }
 
-    /********************************************************************
+    	/********************************************************************
 	*@method		: IsEnoughFuel
 	********************************************************************/
 	bool IsEnoughFuel() { return (mFuelLevel >= mMunimalLEvelToStop); }
-
-
 };
-
 
 #endif //__FUEL_H__
